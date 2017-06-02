a8f04732634cb189e55f42252324eff7bc2f3b11
Amidou Kante <kamdiou@gmail.com>
2017-06-02 14:46:36 -0400

    first row is capable of counting up to 9

--- calculator/main.js
+++ calculator/main.js
@@ -1,9 +1,23 @@
-var row1 = 0;
+var row1 = 0,
+    row2 = 0,
+    row3 = 0,
+    row4 = 0,
+    row5 = 0,
+    row6 = 0,
+    row7 = 0,
+    row8 = 0,
+    row9 = 0;
+
 function counter1(event){
-    name = event.target.name;
-    console.log(name);
-    abacus = document.getElementsByName(name)
-    abacus.className += " clicked";
-    row1+=1;
-    console.log(row1)
+    name = (event);
+    abacus = document.getElementById(name)
+    if(abacus.className === "abacus_circle clicked"){
+        abacus.className = "abacus_circle"
+        row1 = row1 - 1;
+        console.log(row1);
+    }else if(abacus.className === "abacus_circle"){
+        abacus.className += " clicked";
+        row1 = row1 + 1;
+        console.log(row1);
+    }
 }
