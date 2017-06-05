8c37f3fbd8ac4b72c02b700debe12694d79cdfa0
Amidou Kante <kamdiou@gmail.com>
2017-06-05 15:20:44 -0400

    started work on clock

--- clock/main.js
+++ clock/main.js
@@ -1,11 +1,13 @@
-var progressBar;
-progressBar = document.getElementById("timer");
+var set_hour,
+    set_min,
+    set_seconds,
+    set_am_pm;
 
-function startTimer(){
-    for(var i=0; i<100; i++){
-        progressBar.value += i;
-        console.log(progressBar.value)
-    }
+function setAlarm(){
+    set_hour = document.getElementById("set_hour").value;
+    set_min = document.getElementById("set_min").value;
+    set_seconds = document.getElementById("set_seconds").value;
+    set_am_pm = document.getElementById("set_am_pm").value;
+
+    console.log(set_hour, set_min, set_seconds, set_am_pm)
 }
-
-startTimer()
