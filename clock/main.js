57bcc9487e36542c93f19b2bc72d3289d2f7b7a0
Amidou Kante <kamdiou@gmail.com>
2017-06-05 16:25:10 -0400

    progress bar

--- clock/main.js
+++ clock/main.js
@@ -3,6 +3,13 @@ var set_hour,
     set_seconds,
     set_am_pm;
 
+var progressBar;
+
+function checkVal(object){
+    if (object.value.length > object.max.length)
+      object.value = object.value.slice(0, object.max.length)
+}
+
 function setAlarm(){
     set_hour = document.getElementById("set_hour").value;
     set_min = document.getElementById("set_min").value;
@@ -11,3 +18,19 @@ function setAlarm(){
 
     console.log(set_hour, set_min, set_seconds, set_am_pm)
 }
+
+function setDate(){
+ var now = new Date();
+ var seconds = now.getSeconds();
+ var secondsDegress = ((seconds / 60) * 6000);
+ var minutes = now.getMinutes();
+ var minutesDegress = ((minutes / 60) * 6000);
+ var hours = now.getHours();
+ var hoursDegress = (hours * 100) - 100; 
+
+    progressBar = document.getElementById("timer");
+    progressBar.value = seconds;
+
+}
+
+setInterval(setDate, 1000);
