c5c682f84115587e727c78796e180d07fcda62ac
Amidou Kante <kamdiou@gmail.com>
2017-06-05 16:49:31 -0400

    progress bar

--- clock/main.js
+++ clock/main.js
@@ -6,8 +6,7 @@ var set_hour,
 var progressBar,
     progressBar_hour,
     progressBar_min,
-    progressBar_sec,
-    progressBar_ms;
+    progressBar_sec;
 
 function checkVal(object){
     if (object.value.length > object.max.length)
@@ -25,7 +24,6 @@ function setAlarm(){
 
 function setDate(){
  var now = new Date();
- var ms = now.getMilliseconds();
  var seconds = now.getSeconds();
  var secondsDegress = ((seconds / 60) * 6000);
  var minutes = now.getMinutes();
@@ -36,12 +34,10 @@ function setDate(){
     progressBar_hour = document.getElementById("hour_bar");
     progressBar_min = document.getElementById("min_bar");
     progressBar_sec = document.getElementById("sec_bar");
-    progressBar_ms = document.getElementById("ms_bar");
 
     progressBar_hour.value = hours;
     progressBar_min.value = minutes;
     progressBar_sec.value = seconds;
-    progressBar_ms.value = ms;
 
 }
 
