163abbb8d85fba62255830957e406f8b384fec2d
Amidou Kante <kamdiou@gmail.com>
2017-06-08 14:28:01 -0400

    fixed up code

--- animation/main.js
+++ animation/main.js
@@ -1,6 +1,8 @@
 function move_text(){
-    var input_text =document.getElementById("input_text");
+    var input_text = document.getElementById("input_text");
+    var form_body = document.getElementById("form_body");
     var text = input_text.value;
+    form_body.style.display = "none";
     text = text.split(" ");
 
     for(var i = 0; i <text.length; i++){
@@ -17,7 +19,7 @@ function move_text(){
 
     function move(lines){
         for(var i = 0; i<lines.length; i++){
-            lines[i].style.left = Math.random() * 250 + "px";
+            lines[i].style.left = Math.random() * 300 + "px";
             lines[i].style.top = Math.random() * 350 + "px";
             lines[i].style.fontSize = Math.random() * 70 + 'px';
             lines[i].style.color = "rgba(255, 255, 255," + Math.random()+")";
@@ -25,6 +27,7 @@ function move_text(){
         }
     }
 }
+
 function key_trigger(e){
     var keyValue = e.keyCode;
     if(keyValue == "13" || keyValue == "32"){
